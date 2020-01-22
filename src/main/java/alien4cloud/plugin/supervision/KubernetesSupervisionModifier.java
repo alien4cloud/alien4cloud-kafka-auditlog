3f2120ac867c83c7825e2d92dd2e12a348b93a3b
Sébastien LAURENT <sebastien.laurent@atos.net>
2020-01-22 13:31:53 +0100

    First SparkJob annotations

--- src/main/java/alien4cloud/plugin/supervision/KubernetesSupervisionModifier.java
+++ src/main/java/alien4cloud/plugin/supervision/KubernetesSupervisionModifier.java
@@ -6,15 +6,13 @@ import alien4cloud.model.application.ApplicationEnvironment;
 import alien4cloud.model.common.MetaPropertyTarget;
 import alien4cloud.model.common.Tag;
 import alien4cloud.tosca.context.ToscaContext;
-import alien4cloud.tosca.context.ToscaContextual;
 import alien4cloud.utils.PropertyUtil;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import lombok.extern.slf4j.Slf4j;
 import org.alien4cloud.alm.deployment.configuration.flow.FlowExecutionContext;
 import org.alien4cloud.alm.deployment.configuration.flow.TopologyModifierSupport;
-import org.alien4cloud.tosca.model.definitions.AbstractPropertyValue;
+import org.alien4cloud.tosca.model.definitions.ComplexPropertyValue;
 import org.alien4cloud.tosca.model.definitions.ScalarPropertyValue;
 import org.alien4cloud.tosca.model.templates.NodeTemplate;
 import org.alien4cloud.tosca.model.templates.Topology;
@@ -29,7 +27,6 @@ import javax.inject.Inject;
 import java.io.IOException;
 import java.util.Map;
 import java.util.Optional;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -37,6 +34,8 @@ import java.util.stream.Collectors;
 public class KubernetesSupervisionModifier extends TopologyModifierSupport {
 
     private final static String K8S_DEPLOYMENT_RESOURCE = "org.alien4cloud.kubernetes.api.types.DeploymentResource";
+
+    private final static String K8S_SPARK_JOBS = "org.alien4cloud.k8s.spark.jobs.AbstractSparkJob";
 
     private final static String CAS_USAGE = "Cas d'usage";
 
@@ -73,16 +72,81 @@ public class KubernetesSupervisionModifier extends TopologyModifierSupport {
             try {
                 ObjectNode spec = (ObjectNode) mapper.readTree(PropertyUtil.getScalarValue(specProp));
 
-                addMetadata(initialTopology,deployedTopology, node, context,spec);
+                annotateDeployment(initialTopology,deployedTopology, node, context,spec);
 
                 specProp.setValue(mapper.writeValueAsString(spec));
             } catch(IOException e) {
                 log.error("Can't parse json: {}",e);
             }
         }
+
+        for (NodeTemplate node : TopologyNavigationUtil.getNodesOfType(deployedTopology,K8S_SPARK_JOBS,true)) {
+            annotateSparkJob(initialTopology,deployedTopology,node,context);
+        }
     }
 
-    private void addMetadata(Topology initialTopology,Topology deployedTopology,NodeTemplate deploymentNode,FlowExecutionContext context,ObjectNode spec) {
+    private void annotateSparkJob(Topology initialTopology, Topology deployedTopology, NodeTemplate deploymentNode, FlowExecutionContext context) {
+        Application application = context.getEnvironmentContext().get().getApplication();
+        ApplicationEnvironment environment = context.getEnvironmentContext().get().getEnvironment();
+
+        NodeTemplate initialDeployment = getInitialDeployment(deploymentNode,initialTopology);
+
+        // Create context by hand because it needs to be done on initialTopology
+        ToscaContext.Context toscaContext = new ToscaContext.Context(initialTopology.getDependencies());
+
+        // Domaine
+        if (metaAppId != null && application.getMetaProperties() != null) {
+            String value = application.getMetaProperties().get(metaAppId);
+            if (value != null) {
+                log.info("Must Add annotation {}",value);
+                addAnnotationOnSparkJob(deploymentNode,String.format("%s.domaine", configuration.getMetaPrefix()),String.format("%s/%s", CAS_USAGE, value));
+            }
+        }
+
+        // Site
+        addAnnotationOnSparkJob(deploymentNode, String.format("%s.site", configuration.getMetaPrefix()), configuration.getSite());
+
+        // Application
+        if (deployedTopology.getTags()!=null) {
+            Optional<Tag> tag = deployedTopology.getTags().stream().filter(t -> t.getName().equals(TEMPLATE_NAME)).findFirst();
+            if (tag.isPresent()) {
+                addAnnotationOnSparkJob(deploymentNode, String.format("%s.application", configuration.getMetaPrefix()), tag.get().getValue());
+            }
+        }
+
+        // Domaine
+        /*
+        if (initialDeployment != null) {
+            for (NodeTemplate containerNode : TopologyNavigationUtil.getSourceNodesByRelationshipType(initialTopology, initialDeployment, NormativeRelationshipConstants.HOSTED_ON)) {
+                NodeType containerType = toscaContext.getElement(NodeType.class,containerNode.getType(),true);
+
+                String type = ClassUtils.getShortClassName(containerNode.getType()).toLowerCase();
+
+                addLabel(spec, String.format("%s.module.%s", configuration.getMetaPrefix(), map.get(containerNode.getName())), type);
+
+                if (containerNode.getTags()!=null) {
+                    Optional<Tag> tokenTag = containerNode.getTags().stream().filter(t -> t.getName().equals(MLS_TOKEN)).findFirst();
+                    if (tokenTag.isPresent()) {
+                        addLabel(spec, String.format("%s.tokenid.%s", configuration.getMetaPrefix(), map.get(containerNode.getName())), tokenTag.get().getValue());
+                    }
+                }
+            }
+        }*/
+
+        addAnnotationOnSparkJob(deploymentNode, String.format("%s.app_instance", configuration.getMetaPrefix()), String.format("%s-%s", environment.getApplicationId(), environment.getName()));
+    }
+
+    private void addAnnotationOnSparkJob(NodeTemplate deploymentNode,String key,String value) {
+        ComplexPropertyValue prop = (ComplexPropertyValue) deploymentNode.getProperties().get("annotations");
+        prop.getValue().put(key,value);
+    }
+
+    private void addLabelOnSparkJob(NodeTemplate deploymentNode,String key,String value) {
+        ComplexPropertyValue prop = (ComplexPropertyValue) deploymentNode.getProperties().get("labels");
+        prop.getValue().put(key,value);
+    }
+
+    private void annotateDeployment(Topology initialTopology, Topology deployedTopology, NodeTemplate deploymentNode, FlowExecutionContext context, ObjectNode spec) {
         Application application = context.getEnvironmentContext().get().getApplication();
         ApplicationEnvironment environment = context.getEnvironmentContext().get().getEnvironment();
 
@@ -105,9 +169,11 @@ public class KubernetesSupervisionModifier extends TopologyModifierSupport {
         addAnnotation(spec, String.format("%s.site", configuration.getMetaPrefix()), configuration.getSite());
 
         // Application
-        Optional<Tag> tag = deployedTopology.getTags().stream().filter(t -> t.getName().equals(TEMPLATE_NAME)).findFirst();
-        if (tag.isPresent()) {
-            addAnnotation(spec, String.format("%s.application", configuration.getMetaPrefix()), tag.get().getValue());
+        if (deployedTopology.getTags()!=null) {
+            Optional<Tag> tag = deployedTopology.getTags().stream().filter(t -> t.getName().equals(TEMPLATE_NAME)).findFirst();
+            if (tag.isPresent()) {
+                addAnnotation(spec, String.format("%s.application", configuration.getMetaPrefix()), tag.get().getValue());
+            }
         }
 
         // Domaine
@@ -116,12 +182,14 @@ public class KubernetesSupervisionModifier extends TopologyModifierSupport {
                 NodeType containerType = toscaContext.getElement(NodeType.class,containerNode.getType(),true);
 
                 String type = ClassUtils.getShortClassName(containerNode.getType()).toLowerCase();
-                Optional<Tag> tokenTag = containerNode.getTags().stream().filter(t -> t.getName().equals(MLS_TOKEN)).findFirst();
 
                 addLabel(spec, String.format("%s.module.%s", configuration.getMetaPrefix(), map.get(containerNode.getName())), type);
 
-                if (tokenTag.isPresent()) {
-                    addLabel(spec, String.format("%s.tokenid.%s", configuration.getMetaPrefix(), map.get(containerNode.getName())), tokenTag.get().getValue());
+                if (containerNode.getTags()!=null) {
+                    Optional<Tag> tokenTag = containerNode.getTags().stream().filter(t -> t.getName().equals(MLS_TOKEN)).findFirst();
+                    if (tokenTag.isPresent()) {
+                        addLabel(spec, String.format("%s.tokenid.%s", configuration.getMetaPrefix(), map.get(containerNode.getName())), tokenTag.get().getValue());
+                    }
                 }
             }
         }
